83ad4b172027d8f79b86aeb6eeef7378b79b2be4
Javiera <sereno.javiera@gmail.com>
2018-06-28 08:17:57 -0400

    avance TRANS-LAB

--- js/main.js
+++ js/main.js
@@ -1,1 +1,39 @@
-/* Holiii acá va tu código también */
+//funciones Page 1
+validarMail = () => {
+    var emailUser = document.getElementById('email').value;
+    emailRegex = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
+    if (emailRegex.test(emailUser)) {
+      alert('email valido ' + emailUser);
+    } else {
+      alert('email incorrecto ' + emailUser);
+    }
+}
+
+function validarNum(event) {
+    if(event.charCode >= 48 && event.charCode <= 57){
+      return true;
+     }
+     return false;        
+}
+
+function validarLargo() {
+    var claveUser = document.getElementById('clave').value
+    if(claveUser.length <= 8){
+        alert('clave ok!');
+        return true
+    }
+    else{
+        alert('clave incorrecta,debe contener máx 8 números');
+        return false;
+    }
+}
+
+function iniciarSesion() {
+    window.open('index2.html','page2','_parent');
+}
+ 
+
+//Funciones Page 2
+
+
+//funciones Page 3
